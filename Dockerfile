965c2aee41d5941a517ca9077321d284cde301cd
rmkn <bubblun@gmail.com>
2018-03-12 16:22:41 +0900

    poweradminまわりの変更

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,15 @@
 FROM rmkn/php
 MAINTAINER rmkn
 RUN yum -y install epel-release
+RUN rpm --import http://rpms.famillecollet.com/RPM-GPG-KEY-remi
 RUN rpm -Uvh http://rpms.famillecollet.com/enterprise/remi-release-6.rpm
 RUN yum -y install php-mcrypt php-pdo php-mysql mysql-server pdns pdns-backend-mysql
 
 RUN curl -o /tmp/poweradmin.tar.gz -SL http://jaist.dl.sourceforge.net/project/poweradmin/poweradmin-2.1.7.tgz \
         && tar xzf /tmp/poweradmin.tar.gz -C /var/www/html/ --strip=1
+
+RUN cp /var/www/html/inc/config-me.inc.php /var/www/html/inc/config.inc.php
+RUN chown -R apache. /var/www/html/*
 
 COPY entrypoint.sh /
 COPY powerdns.sql init.sh /tmp/
--- init.sh
+++ init.sh
@@ -1,13 +1,5 @@
 #!/bin/sh
 
-mv /var/www/html/inc/config-me.inc.php /var/www/html/inc/config.inc.php
-sed -i -e "s/^\$db_host.*$/\$db_host = 'localhost';/" /var/www/html/inc/config.inc.php
-sed -i -e "s/^\$db_port.*$/\$db_port = '3306';/" /var/www/html/inc/config.inc.php
-sed -i -e "s/^\$db_user.*$/\$db_user = 'powerdns';/" /var/www/html/inc/config.inc.php
-sed -i -e "s/^\$db_pass.*$/\$db_pass = 'powerdns';/" /var/www/html/inc/config.inc.php
-sed -i -e "s/^\$db_name.*$/\$db_name = 'powerdns';/" /var/www/html/inc/config.inc.php
-sed -i -e "s/^\$db_type.*$/\$db_type = 'mysql';/" /var/www/html/inc/config.inc.php
-
 sed -i -e 's/^launch.*/launch=gmysql/' /etc/pdns/pdns.conf
 cat << EOS >> /etc/pdns/pdns.conf
 gmysql-host=localhost
